2df8c37214cbbd102c34769fa31b727b52b07a02
Anderson Sanches <andycds@gmail.com>
2022-08-12 21:51:12 -0300

    Primeiro teste concluído com sucesso

--- Empregado.java
+++ Empregado.java
@@ -39,7 +39,26 @@ public class Empregado {
         this.tipo = tipo;
     }
     
-    public int tipo() {
+    public int getTipo() {
         return this.tipo;
     }
+
+    public void setSalario (double salario){
+this.salario = salario;
+}
+public double getSalario (){
+return this.salario;
+}
+public void setComissao (double comissao){
+this.comissao = comissao;
+}
+public double getComissao (){
+return this.comissao;
+}
+public void setBonus (double bonus){
+this.bonus = bonus;
+}
+public double getBonus (){
+return this.bonus;
+}
 }
